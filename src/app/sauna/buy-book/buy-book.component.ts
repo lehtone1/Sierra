baba35cdc438aae49493611920c52320894c2568
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-08-13 22:15:35 +0300

    BuyBook email added

--- src/app/sauna/buy-book/buy-book.component.ts
+++ src/app/sauna/buy-book/buy-book.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyBookComponent implements OnInit {
 
+  name = '';
+  apartment = '';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  updateName(event: Event) {
+    this.name = (<HTMLInputElement>event.target).value;
+  }
+
+  updateApartment(event: Event) {
+    this.apartment = (<HTMLInputElement>event.target).value;
+  }
+
+  public buyBookMail() {
+    const link = 'mailto:eero.lehtonen@aalto.fi'
+            + '?subject=' + encodeURI('Sierra kirjan osto')
+            + '&body=' + encodeURI(`Nimi: ${this.name} \n Asunto: ${this.apartment}`)
+    ;
+
+    window.location.href = link;
+}
 
 }

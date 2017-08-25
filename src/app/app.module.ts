045ecfdd5ec31ee066e94ecf132fd91cdeccf913
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-08-26 01:45:33 +0300

    rearangement and font adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationBarComponent } from './header/navigation-bar/navigation-bar.component';
 import { AppRoutingModule, routingComponents } from './app.routing';
-import { SaunaReservationComponent } from './sauna/sauna-reservation/sauna-reservation.component';
+import { SaunaReservationComponent } from './other/sauna-reservation/sauna-reservation.component';
 import { AnnouncementsListComponent } from './announcement/announcements-list/announcements-list.component';
-import { LinkListComponent } from './sauna/link-list/link-list.component';
-import { GarageComponent } from './sauna/garage/garage.component';
-import { BuyBookComponent } from './sauna/buy-book/buy-book.component';
+import { LinkListComponent } from './other/link-list/link-list.component';
+import { GarageComponent } from './other/garage/garage.component';
+import { BuyBookComponent } from './other/buy-book/buy-book.component';
 import { FooterComponent } from './footer/footer.component';
 
 

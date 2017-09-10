543bb97b5c38632f27c7410abcde1d0affe20484
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-09-10 18:32:50 +0300

    Updated contacts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,9 +13,9 @@ export class ContactComponent implements OnInit {
   new Contact('Hallituksen puheenjohtaja', 'Heikki Lehtonen', '050 1311', 'heikki@lehtonen.fi'),
   new Contact('Hallituksen Varapuheenjohtaja', 'Erkki Yrjölä',  '0500 814 811', 'e.yrjola@finneire.fi'),
   new Contact('Hallituksen jäsen', 'Sari Perttunen', '040 488 9444', 'sari.perttunen@reima.com'),
-  new Contact('Hallituksen jäsen', 'Martin Relander', '050 332 3652', 'martin.relander@gmail.com'),
+  new Contact('Hallituksen jäsen', 'Martin Relander', '040 523 4896', 'martin.relander@gmail.com'),
   new Contact('Hallituksen jäsen', 'Hannu Väänänen', '050 332 3652', 'hannu.vaananen@fi.abb.com'),
-  new Contact('Hallituksen Varajäsen', 'Esa Salonen', '0400 503 385', 'esa.salonen@peltonenlaw.fi'),
+  new Contact('Hallituksen Varajäsen', 'Esa Salonen', '0400 503 385', 'esa.salonen@dlapiper.com'),
   new Contact('Hallituksen Varajäsen', 'Timo Suominen', '040 522 7176', 'suominen.timoj@gmail.com')
   ];
 
